43d23a5bd4c8d4f3ec768a4d38886d398f24add2
mr-willie <han.junhui.06@outlook.com>
2026-01-03 01:55:11 +0800

    2026/01/02/16:29 - 重新组织语句

--- 计算并输出列表元素的平均值.py
+++ 计算并输出列表元素的平均值.py
@@ -1,5 +1,5 @@
 yonghushuru = input("输入列表数字，每个元素用空格隔开: ")
 liebiao = [float(num) for num in yonghushuru.split()] #把字符串转化成可计算的数字
 zonghe = sum(liebiao)
-average = zonghe / len(liebiao)
-print(f"平均值为: {average}")
+pingjun = zonghe / len(liebiao)
+print(f"平均值为: {pingjun}")
